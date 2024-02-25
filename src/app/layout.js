129bc47c47ee2aa0e0249b068c750cd6253d3b06
Jackie Tsien <jackie10673311@gmail.com>
2024-02-25 17:47:33 +0800

    Add new components and update existing ones

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,9 @@ import {
   Spline_Sans_Mono,
 } from 'next/font/google';
 import clsx from 'clsx';
+import RespectMotionPreferences from '@/components/RespectMotionPreferences';
 
-import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
+import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from '@/constants';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
@@ -24,23 +25,30 @@ const monoFont = Spline_Sans_Mono({
   variable: '--font-family-mono',
 });
 
+export const metadata = {
+  title: BLOG_TITLE,
+  description: "A wonderful blog about JavaScript"
+}
+
 function RootLayout({ children }) {
   // TODO: Dynamic theme depending on user preference
   const theme = 'light';
 
   return (
-    <html
-      lang="en"
-      className={clsx(mainFont.variable, monoFont.variable)}
-      data-color-theme={theme}
-      style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
-    >
-      <body>
-        <Header theme={theme} />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <RespectMotionPreferences>
+      <html
+        lang="en"
+        className={clsx(mainFont.variable, monoFont.variable)}
+        data-color-theme={theme}
+        style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+      >
+        <body>
+          <Header theme={theme} />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </RespectMotionPreferences>
   );
 }
 
